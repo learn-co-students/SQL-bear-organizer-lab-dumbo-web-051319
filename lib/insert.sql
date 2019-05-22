c2e65fda89884b92a5ced2c387ce54e04ab88e09
Tess Neau <tessneau@gmail.com>
2019-05-22 17:39:46 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr.Chocolate", 25, "female", "blue", "calm", true),
+("Rowdy", 25, "female", "blue", "angry", false),
+("Tabitha", 25, "male", "brown", "tired", false),
+("Seargent Brown", 25, "non-binary", "blue-ish", "hyper", true),
+("Melissa", 25, "female", "yellow", "hungry", false),
+("Grinch", 25, "male", "white", "sick", false),
+("Wendy", 25, "intersex", "black", "annoyed", true),
+(NULL, NULL, "female", "black", "stressed", true);
