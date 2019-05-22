7ab62cb86437fedb5f3feece0e42cb4c27ae8c55
tommaclean <tom.j.maclean@gmail.com>
2019-05-22 19:03:43 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+-- (1,"Mr. Chocolate", 30, "M", "brown", "nice guy", 1),
+-- (2,"Rowdy", 4, "F", "black", "good girl", 1),
+-- (3,"Tabitha", 5, "M", "grey", "angry", 1),
+-- (4,"Sargeant Brown", 6, "F", "black", 0),
+-- (5,"Melissa", 7, "M", "brown", "agitated", 1),
+-- (6,"Grinch", 2, "M", "green", "friendly" 0),
+-- (7,"Wendy", 22, "F", "brown", "good girl", 0),
+-- (8,null, 13, "M", "black", "downright bad bear", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
