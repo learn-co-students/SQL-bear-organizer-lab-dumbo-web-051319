1454056ade7ce2aff8dc394b457c2c9f0b721100
“hannah6530” <hannah6530@gmail.com>
2019-05-24 14:28:33 -0400

    Done.

--- lib/seed.sql
+++ lib/seed.sql
@@ -1,6 +1,6 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament,  alive) VALUES
 (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
-(2,"Rowdy", 10, "M", "black", "intense", 1),
+(2,"Rowdy", 10, "M", "black", "intense",  1),
 (3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
 (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
 (5,"Melissa", 13, "F", "dark brown", "goofy", 1),
